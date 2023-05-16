cce6d4593f9dd0bd6b604911fbafaadb920bb62d
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-05-16 17:55:19 +0200

    Implemented `filter_episode_transcript`

--- src/qa_system/data/process_data.py
+++ src/qa_system/data/process_data.py
@@ -1,4 +1,4 @@
-"""Filters the Episode-i.txt files from the data/raw/transripts folder and saves them
+"""Filters the Episode-i.txt files from the data/raw/transcripts folder and saves them
 to data/processed/transcripts folder.
 
 These text files follow the format:
@@ -34,5 +34,16 @@ def filter_episode_transcript(raw_episode_transcript: str) -> str:
     Returns:
         filtered_episode_transcript (str): The filtered episode transcript.
     """
+    split_newline = raw_episode_transcript.split("\n")
+    filtered_episode_transcript = ""
+    for line in split_newline:
+        # Remove the brackets and the text inside them.
+        line = re.sub(r"\[(.*?)\]", "", line)
+        # If the line starts with a timestamp, add a new line.
+        if re.match(r"^\d+.\d+", line):
+            filtered_episode_transcript += "\n"
+        # In the line, insert a comma after the timestamp.
+        line = re.sub(r"(\d+.\d+)", r"\1\t", line)
+        filtered_episode_transcript += line
 
-
+    return filtered_episode_transcript[1:]  # Remove the first newline.
