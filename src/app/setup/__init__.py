5c539501c95208055ae766f57af45b799c49df85
Pabloo22 <pablete.arino@gmail.com>
2023-06-02 18:48:28 +0200

    Change project structure

--- src/app/setup/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This module contains the functions neccessary to fully install the app.
-
-The installation process consists of the following steps:
-1. Download the models.
-2. Preprocess the data.
-3. Convert the data to vectors.
-"""
-from preprocessing import preprocess_data, get_directory
--- src/app/setup/download_transcripts.py
+++
@@ -1,1 +0,0 @@
-"""Code to download the transcripts from YouTube."""
