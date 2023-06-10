36295d37d79b7e700e37632cd19b74304db91ce3
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-06-10 18:31:48 +0200

    Added docs and type hints

--- ask_youtube_playlists/data/download_transcripts.py
+++ ask_youtube_playlists/data/download_transcripts.py
@@ -5,8 +5,16 @@ import json
 import logging
 import youtube_transcript_api
 
+from typing import Dict, List, Union
 
-def get_playlist_info(url: str):
+
+def get_playlist_info(url: str) -> Dict[str, str]:
+    """Gets the video IDs and titles from a YouTube playlist.
+    Args:
+        url (str): The URL of the YouTube playlist.
+    Returns:
+        Dict[str, str]: A dictionary with the video titles as keys and the video IDs as values.
+        """
     playlist = pytube.Playlist(url)
 
     # Dict to hold title-ID pairs
@@ -21,7 +29,16 @@ def get_playlist_info(url: str):
 def download_transcript(video_title: str,
                         video_id: str,
                         output_path: pathlib.Path,
-                        logger: logging.Logger = None):
+                        logger: logging.Logger = None) -> None:
+    """Download the transcript of a YouTube video.
+    Args:
+        video_title (str): The title of the YouTube video.
+        video_id (str): The ID of the YouTube video.
+        output_path (pathlib.Path): The path to the output file.
+        logger (logging.Logger): The logger.
+    Raises:
+        Exception: If the transcript cannot be downloaded.
+    """
     try:
         # Download transcript with youtube_transcript_api
         transcript = youtube_transcript_api.YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'en-US'])
@@ -44,21 +61,39 @@ def download_transcript(video_title: str,
             logger.error(f'An error occurred: {str(e)}')
 
 
-def download_playlist(url):
+def download_playlist(url: str, data_path: pathlib.Path) -> None:
+    """Download the transcripts of a YouTube playlist.
+    Args:
+        url (str): The URL of the YouTube playlist.
+        data_path (pathlib.Path): The path to the data directory.
+    """
     video_id_dict = get_playlist_info(url)
 
     for i, (video_title, video_id) in enumerate(video_id_dict.items()):
-        output_file = 'Episode_' + str(i + 1) + '.json'
+        output_file = data_path / 'raw' / f'Episode_{str(i + 1)}.json'
         download_transcript(video_title, video_id, output_file)
 
 
-def _replace_newlines(json_file: dict):
-    """Replace \n with a space"""
+def _replace_newlines(json_file: dict) -> None:
+    """Replace \n with a space
+    Args:
+        json_file (dict): The JSON file.
+        """
     for segment in json_file['transcript']:
         segment['text'] = segment['text'].replace('\n', ' ')
 
 
-def create_chunked_data(file, max_chunk_size, min_overlap_size):
+def create_chunked_data(file: str,
+                        max_chunk_size: int,
+                        min_overlap_size: int) -> List[Dict[str, Union[str, List[str]]]]:
+    """Create chunked data from a JSON file.
+    Args:
+        file (str): The path to the JSON file.
+        max_chunk_size (int): The maximum size of a chunk.
+        min_overlap_size (int): The minimum size of the overlap between two chunks.
+    Returns:
+        List[Dict[str, Union[str, List[str]]]]: A dictionary with the chunked data.
+        """
     with open(file, 'r') as f:
         json_file = json.load(f)
 
@@ -94,7 +129,9 @@ def create_chunked_data(file, max_chunk_size, min_overlap_size):
             [segment['text'] for segment in json_file['transcript'][chunk_index[0]:chunk_index[1] + 1]]),
         'start': json_file['transcript'][chunk_index[0]]['start'],
         'duration': sum(
-            [segment['duration'] for segment in json_file['transcript'][chunk_index[0]:chunk_index[1] + 1]])}
+            [segment['duration'] for segment in json_file['transcript'][chunk_index[0]:chunk_index[1] + 1]]),
+        'url': json_file['url'],
+        'title': json_file['title']}
         for chunk_index in chunks_indices]
 
     return chunks
