d9c3cdc178437df405c33bf6842d0a74ebc4ce60
Pabloo22 <pablete.arino@gmail.com>
2023-06-13 21:46:04 +0200

    Create answer.py (empty)

--- ask_youtube_playlists/question_answering/question_answerer.py
+++
@@ -1,9 +0,0 @@
-"""Facade class that uses the Question Answering system"""
-import pathlib
-
-
-class QuestionAnswerer:
-
-    def __init__(self,
-                 data_path: pathlib.Path):
-        self.data_path = data_path
