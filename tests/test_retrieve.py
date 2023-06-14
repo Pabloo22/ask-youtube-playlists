7d838b19ac2dfc48bf4c71a23d2745e3467fff80
Álvaro Laguna <72698468+alvarola17@users.noreply.github.com>
2023-06-14 19:40:52 +0200

    Added test_retrieve.py, test_create_db.py, small fix on retrieve.py and create_db.py

--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -10,9 +10,10 @@ import os
 from ask_youtube_playlists.data_processing import (create_vectorstore,
                                                    save_vectorstore,
                                                    load_vectorstore)
+from ask_youtube_playlists.question_answering.retrieve import retrieve
 
 
-def test_create_vectorstore():
+def test_retrieve():
     test_persist_directory = str(pathlib.Path("tests") / "test_db")
     # Clear the test directory if it exists
     if pathlib.Path(test_persist_directory).exists():
@@ -20,16 +21,16 @@ def test_create_vectorstore():
         os.system(f"mkdir {test_persist_directory}")
 
     documents = [
-        Document(page_content="This is the first document.",
+        Document(page_content="The meaning of life is 42.",
                  metadata={'title': 'first'}),
-        Document(page_content="This is the second document.",
+        Document(page_content="It is sunny today, good life.",
                  metadata={'title': 'second'}),
-        Document(page_content="This is the third document.",
+        Document(page_content="I like trains.",
                  metadata={'title': 'third'}),
     ]
     test_persist_directory = str(pathlib.Path("tests") / "test_db")
     vector_store: vectorstores.Chroma = create_vectorstore(
-        "msmarco-MiniLM-L-6-v3",
+        "msmarco-distilbert-base-tas-b",
         documents,
         vector_store_type="chroma-db",
         persist_directory=test_persist_directory)
@@ -37,15 +38,10 @@ def test_create_vectorstore():
     save_vectorstore(vector_store)
     loaded_vector_store = load_vectorstore(test_persist_directory)
 
-    # Check thad the loaded vector has the documents inside
-    get_loaded_documents = loaded_vector_store.get()
-    loaded_page_content = get_loaded_documents['documents']
-    loaded_metadatas = get_loaded_documents['metadatas']
-    loaded_documents = zip(loaded_page_content, loaded_metadatas)
-    for doc_num, (page_content, metadata) in enumerate(loaded_documents):
-        assert page_content == documents[doc_num].page_content
-        assert metadata == documents[doc_num].metadata
+    dict_vector_store = {'vector_store': vector_store}
 
+    retrieved_document = retrieve("What is the meaning of life?",
+                                  dict_vector_store,
+                                  1)
 
-if __name__ == "__main__":
-    pytest.main(["-vv", "test_create_db.py"])
+    assert retrieved_document[0].document == documents[0]
