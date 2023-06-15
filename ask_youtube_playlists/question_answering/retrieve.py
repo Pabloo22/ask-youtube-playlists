092e6b1b75b6924a83066777ade7451c5a9e368f
Pabloo22 <pablete.arino@gmail.com>
2023-06-15 11:08:37 +0200

    Minor fixes Retriever class (#22)

--- ask_youtube_playlists/question_answering/retrieve.py
+++ ask_youtube_playlists/question_answering/retrieve.py
@@ -2,12 +2,11 @@
 for a given question."""
 import pathlib
 
+from typing import List, NamedTuple
+
 import numpy as np
 import yaml
 
-from typing import Dict, List, NamedTuple, Tuple
-
-from langchain import vectorstores
 from langchain.schema import Document
 
 from ask_youtube_playlists.data_processing import (
@@ -89,53 +88,9 @@ class Retriever:
         denominator = question_embedding_norm * document_embedding_norm
         return np.dot(question_embedding, document_embedding) / denominator
 
-    def _get_similarities(self,
-                          question_embedding: np.ndarray,
-                          video_embedding: np.ndarray) -> List[float]:
-        """Returns a list of relevance scores for each document in the
-        video.
-
-        The score should be in the range [0, 1], where 1 means that the
-        document is very relevant to the question and 0 means that it is not
-        relevant at all.
-
-        Args:
-            question_embedding (np.ndarray): The embedding of the question.
-                The shape should be (embedding_size,).
-            video_embedding (np.ndarray): The embedding of the video. The
-                shape should be (n_documents_in_the_video, embedding_size).
-        """
-        scores = []
-        for document_embedding in video_embedding:
-            cosine_distance = self.cosine_distance(question_embedding,
-                                                   document_embedding)
-            scores.append(1 - cosine_distance)
-        return scores
-
-    def _get_most_relevant_documents(self,
-                                     scores: List[List[float]],
-                                     n_documents: int
-                                     ) -> List[Tuple[Document, float]]:
-        """Returns the most relevant documents with their relevance score.
-
-        Args:
-            scores (List[List[float]]): A list of lists of relevance scores.
-                The outer list contains the relevance scores for each video.
-                The inner list contains the relevance scores for each document
-                in the video.
-            n_documents (int): The number of documents to retrieve.
-        """
-        documents = []
-        for video_documents, video_scores in zip(self.documents, scores):
-            for document, score in zip(video_documents, video_scores):
-                documents.append((document, score))
-
-        documents.sort(key=lambda x: x[1], reverse=True)
-        return documents[:n_documents]
-
-    def _retrieve(self,
-                  question: str,
-                  n_documents: int) -> List[DocumentInfo]:
+    def retrieve_from_playlist(self,
+                               question: str,
+                               n_documents: int) -> List[DocumentInfo]:
         """Retrieves the most relevant documents with their relevance score.
 
         Args:
@@ -144,21 +99,30 @@ class Retriever:
         """
         n_documents = min(n_documents, self.total_number_of_documents)
 
-        question_embedding = self.embedding_model.embed_query(question)
-        question_embedding = np.array(question_embedding)
-
-        scores = []
-        for video_embedding in self.video_embeddings:
-            video_scores = self._get_similarities(question_embedding,
-                                                  video_embedding)
-            scores.append(video_scores)
-
-        documents = self._get_most_relevant_documents(scores, n_documents)
         playlist_name = self.embedding_directory.parent.name
 
-        result = [DocumentInfo(document, score, playlist_name)
-                  for document, score in documents]
-        return result
+        question_embedding = self.embedding_model.embed_query(question)
+        question_embedding = np.array(question_embedding)  # type: ignore
+
+        document_infos = []
+        for video_documents, video_embedding in zip(self.documents,
+                                                    self.video_embeddings):
+            iterator = zip(video_documents, video_embedding)
+            for i, (document, document_embedding) in enumerate(iterator):
+                if document.metadata["index"] != i:
+                    raise ValueError("The index of the document does not match"
+                                     " its position in the list.")
+
+                score = 1 - self.cosine_distance(
+                    question_embedding, document_embedding  # type: ignore
+                )
+                document_info = DocumentInfo(document=document,
+                                             score=score,
+                                             playlist_name=playlist_name)
+                document_infos.append(document_info)
+
+        document_infos.sort(key=lambda x: x.score, reverse=True)
+        return document_infos[:n_documents]
 
     @classmethod
     def retrieve(cls,
@@ -188,50 +152,10 @@ class Retriever:
         """
         document_infos = []
         for retriever in retrievers:
-            document_infos.extend(retriever._retrieve(question, n_documents))
+            document_infos.extend(retriever.retrieve_from_playlist(
+                question, n_documents
+                )
+            )
 
         document_infos.sort(key=lambda x: x.score, reverse=True)
         return document_infos[:n_documents]
-
-
-def retrieve(question: str,
-             vectorstores_dict: Dict[str, vectorstores.VectorStore],
-             n_documents: int) -> List[DocumentInfo]:
-    """Deprecated.
-
-    Retrieves the most relevant documents with their relevance score and
-    the playlist they belong to.
-
-    This function retrieves documents in two steps:
-
-    1. Extracts the most relevant documents from each vectorstore in
-    `vectorstores_dict`.
-
-    2. Ranks the retrieved documents from all vectorstores and returns the
-    most relevant ones, in addition to their relevance score and the playlist
-    they belong to.
-
-    Args:
-        question (str): The question posed by the user.
-        vectorstores_dict (Dict[str, langchain.vectorstores.VectorStore]): A
-            dictionary mapping playlist names to their respective vectorstores.
-        n_documents (int): The number of documents to retrieve.
-
-    Returns:
-        list: A list of named tuples, each containing the document, its
-        relevance score and the playlist it belongs to. The list is sorted in
-        descending order by relevance score.
-    """
-    retrieved_documents = []
-    for playlist_name, vectorstore in vectorstores_dict.items():
-        retrieval = vectorstore.similarity_search_with_relevance_scores(
-            question, n_documents
-        )
-        for document, score in retrieval:
-            retrieved_documents.append(DocumentInfo(document,
-                                                    score,
-                                                    playlist_name))
-
-    # Sort the documents by relevance score
-    retrieved_documents.sort(key=lambda x: x.score)
-    return retrieved_documents[:n_documents]
